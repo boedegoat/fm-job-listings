20c0456b7302c993d8a97cae800ba8df6e258cc4
boedegoat <bhremada.fka@gmail.com>
2022-05-12 20:26:37 +0700

    add h1 sr-only

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Jobs from './components/Jobs'
 const App = () => {
     return (
         <main className='bg-cyan-light-(bg) min-h-screen'>
+            <h1 className='sr-only'>Job Listings</h1>
             <Header />
             <Filter />
             <Jobs />
